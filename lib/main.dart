29d423875dfe31995c96b926d4fcc1f61cdfd2cd
manzhakdotcom <manzhak.com@gmail.com>
2023-04-07 15:19:50 +0300

    add a little bit styles

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,33 @@ class MyApp extends StatelessWidget {
       'What\'s your favorite color?',
       'What\'s your favorite animal?',
     ];
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My App'),
         ),
-        body: Column(
-          children: const <Widget>[
-            Text('The question!'),
-            MaterialButton(
-              onPressed: null,
-              child: Text('Answer 1'),
-            ),
-            MaterialButton(
-              onPressed: null,
-              child: Text('Answer 2'),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text(
+                'The question!',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              ElevatedButton(
+                style: style,
+                onPressed: () {},
+                child: const Text('Answer 1'),
+              ),
+              ElevatedButton(
+                style: style,
+                onPressed: () {},
+                child: const Text('Answer 2'),
+              ),
+            ],
+          ),
         ),
       ),
     );
